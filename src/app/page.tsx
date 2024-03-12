9ef8c53fff2dc2c7eed4423d852105d7a8017758
Eric Dudley <17283326+ericdudley@users.noreply.github.com>
2024-03-12 09:50:17 -0700

    feat: Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,154 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { MdSettings } from "react-icons/md";
+
+/**
+ *
+ * @param maxTiles
+ * @param minSize
+ * @returns n, m, tileSize to fit the screen
+ */
+function calculateGridSize(maxTiles: number, minSize: number) {
+  const { innerWidth, innerHeight } = window;
+  const tileSize = Math.max(
+    minSize,
+    Math.min(innerWidth / maxTiles, innerHeight / maxTiles),
+  );
+  const n = Math.floor(innerHeight / tileSize);
+  const m = Math.floor(innerWidth / tileSize);
+  return { n, m, tileSize };
+}
+
+
+function generateColorGrid(
+  n: number,
+  m: number,
+  angleoff: number,
+): string[][] {
+  const grid: string[][] = [];
+  const centerX = (n - 1) / 2;
+  const centerY = (m - 1) / 2;
+  // const maxRadius = Math.sqrt(centerX ** 2 + centerY ** 2);
+
+  for (let i = 0; i < n; i++) {
+    const row: string[] = [];
+    for (let j = 0; j < m; j++) {
+      // Calculate radial coordinates
+      const dx = i - centerX;
+      const dy = j - centerY;
+      const angle = Math.atan2(dy, dx) * (180 / Math.PI) + angleoff; // Normalize to [0, 360]
+      // const distance = Math.sqrt(dx ** 2 + dy ** 2);
+      const hue = Math.floor(angle % 360);
+
+      // USE THIS FOR THE GAME
+      // let saturation = 80; 
+      // let lightness = 60;
+      let saturation = 20;
+      let lightness = 10;
+
+      row.push(`hsl(${hue}, ${saturation}%, ${lightness}%)`);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+function ColorGrid() {
+  const { n, m, tileSize } = calculateGridSize(24, 45);
+  const [hue, setHue] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHue((hue) => (hue + 5) % 360);
+    }, 64);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-base-100">
+      {generateColorGrid(n, m, hue).map((row, i) => (
+        <div className="flex flex-row">
+          {row.map((color, j) => (
+            <div
+              key={j}
+              style={{
+                aspectRatio: "1/1",
+                width: `${tileSize}px`,
+                height: `${tileSize}px`,
+              }}
+            >
+              <div
+                className="w-full h-full transition-all hover:scale-[2.0] hover:border-2 hover:border-black"
+                style={{ backgroundColor: color }}
+              />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main
+      className="flex min-h-screen flex-col items-center justify-between p-8"
+      data-theme="dark"
+    >
+      <ColorGrid />
+
+      <div className="z-10 max-w-xl flex flex-col gap-8">
+        <h1 className="text-8xl font-bold text-center">Color Hint</h1>
+        <p>
+          A color guessing game where everyone gets a turn to give hints while
+          the others guess the color they're describing.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div>
+          <div className="flex flex-row items-center gap-4">
+            <MdSettings className="w-8 h-8" />
+            <h2 className="text-4xl font-bold">Settings</h2>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="form-control">
+              <div className="label">
+                <span className="label-text">Rounds</span>
+              </div>
+              <input
+                type="number"
+                placeholder="Rounds"
+                min="1"
+                max="10"
+                className="input"
+              />
+            </div>
+            <div className="form-control">
+              <div className="label">
+                <span className="label-text">Hints per Round</span>
+              </div>
+              <input
+                type="number"
+                placeholder="Hints per Round"
+                min="1"
+                max="10"
+                className="input"
+              />
+            </div>
+            <div className="form-control">
+              <div className="label">
+                <span className="label-text">Guesses per Round</span>
+              </div>
+              <input
+                type="number"
+                placeholder="Guesses per Round"
+                min="1"
+                max="10"
+                className="input"
+              />
+            </div>
+            <button className="btn btn-lg mt-4">
+              Create Lobby
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
---
+++ src/app/types.ts
@@ -0,0 +1,43 @@
+// All types for the Color Hint game
+
+export type Color = {
+  name: string;
+  hex: string;
+};
+
+export type Player = {
+  id: string;
+  name: string;
+};
+
+export type GameSettings = {
+  rounds: number;
+  hintsPerPlayerRound: number[];
+  guessesPerPlayerRound: number[];
+};
+
+export type Guess = {
+  playerId: string;
+  guess: string;
+};
+
+export type GameStatus = "lobby" | "playing";
+export type GameRound = {
+  round: number;
+  playerRounds: PlayerRound[];
+};
+
+export type PlayerRound = {
+  hintingPlayerId: string;
+  color: string;
+  hints: string[];
+  guesses: Guess[];
+};
+
+export type Game = {
+  lobbyCode: string;
+  players: Player[];
+  settings: GameSettings;
+  status: GameStatus;
+  rounds: GameRound[];
+};
